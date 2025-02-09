08a5d688a2f4ea7dc678d30d7db9065025a84bad
Ebadur Rahman <ebadurshohag@gmail.com>
2025-02-10 00:49:51 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "btn-primary": "#E76F51",
+        "warm-coral-transparent": "#E76F511A",
+        "blue-primary": "#23A6F0",
+        "dark-gray": "#6C6C6C",
+        "light-gray": "#7E848C",
+        "charcoal-black": "#383838",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
